cb89d7ee3a1c79093629f9e1f4439e0bdc9648cf
Paulo Sérgio Vieira <paulosv@lnxcit017578.cit>
2019-08-07 16:41:09 -0300

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>      
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center">Hello World</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
