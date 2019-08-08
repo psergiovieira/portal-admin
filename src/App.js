7bc318fe8e10e025d30b2fdfc13afbf74a5d67cd
Paulo Sérgio Vieira <paulosv@lnxcit017578.cit>
2019-08-08 13:29:58 -0300

    create login form anmd redirect with a message

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            <h1 className="text-center">Hello World</h1>
+            <h1 className="text-center">Hello World {props.value}</h1>
           </div>
         </div>
       </div>
