24596084d1463b5af91ba7eec65ae5d1faa1d32e
Jean Philippe Moreira <jeanp@lnxcit019444.cit>
2019-08-16 14:06:23 -0300

    Test check change value input pwd and login

--- src/__tests__/login.spec.js
+++ src/__tests__/login.spec.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { LoginPage }  from '../LoginPage/LoginPage';
 import { store } from '../_helpers/store';
-import { Provider } from "react-redux";
 
 
 
 describe('Teste unit / Component login', () => {
+    const pwdId         = 'input[id="password"]';
+    const loginId       = 'input[id="login"]';
+    const idButtonLogin = 'input[id="idButtonLogin"]'
+    const idLoginError  = 'div[id="idErrorLogin"]'
+
     const user = {
         id: 1,
         username: "PortalAdmin",
@@ -29,45 +33,55 @@ describe('Teste unit / Component login', () => {
 
     it('test check placeHolder input login ', () => {
         const namePlaceHolder = 'login';
-        expect(component.find('input[id="login"]').prop('placeholder')).toBe(namePlaceHolder);
+        expect(component.find(loginId).prop('placeholder')).toBe(namePlaceHolder);
     });
 
     it('test check placeHolder input password ', () => {
 
         const namePlaceHolder = 'password';
-        expect(component.find('input[id="password"]').prop('placeholder')).toBe(namePlaceHolder);
+        expect(component.find(pwdId).prop('placeholder')).toBe(namePlaceHolder);
     });
 
     it('test check placeHolder input type button ', () => {
 
         const typeButton = 'button';
-        expect(component.find('input[id="idButtonLogin"]').prop('type')).toBe(typeButton);
+        expect(component.find(idButtonLogin).prop('type')).toBe(typeButton);
     });
 
     it('test click button show menssage <Error! Login or Password>', () => {
-        component.find('input[id="idButtonLogin"]').simulate('click');
-        expect(component.find('div[id="idErrorLogin"]').text()).toBe("Error! Login or Password");
-        
+        const exptectedMessage = "Error! Login or Password"
+        component.find(idButtonLogin).simulate('click');
+        expect(component.find(idLoginError).text()).toBe(exptectedMessage); 
     });
 
     it('test not click button dont show menssage <Error! Login or Password>', () => {
         const componentLoadNew = mount(<LoginPage store={store} loggingIn={user} {...props} />)
-        expect(componentLoadNew.find('div[id="idErrorLogin"]').isEmpty()).toBe(true);
-        
+        expect(componentLoadNew.find(idLoginError).isEmpty()).toBe(true);
     });
 
-    // it('test change value input password ', () => {
-    //     const textValueInitial = '';
-    //     expect(component.find('input[id="password"]').instance().value).toBe(textValueInitial);
-        
-    //     const event = {target: {value: "1234"}};
-    //     component.find('input[id="password"]').simulate('change', event);;
-        
-        
+    it('test change value input password ', () => {
 
-    //     const textValueFinal = '1234';
-    //     expect(component.find('input[id="password"]').instance().value).toBe(textValueFinal);
-    // });
+        const pwd = component.find(pwdId)
+        const textValueInitial = '';
+        expect(pwd.instance().value).toBe(textValueInitial);
+
+        const textValueFinal = '1234';
+        pwd.instance().value = textValueFinal;
+        pwd.simulate('change');
+        expect(pwd.instance().value).toBe(textValueFinal);
+    });
+
+    it('test change value input login ', () => {
+
+        const loginInput = component.find(loginId)
+        const textValueInitial = '';
+        expect(loginInput.instance().value).toBe(textValueInitial);
+
+        const textValueFinal = '1234';
+        loginInput.instance().value = textValueFinal;
+        loginInput.simulate('change');
+        expect(loginInput.instance().value).toBe(textValueFinal);
+    });
 
     
 
